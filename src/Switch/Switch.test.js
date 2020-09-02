e241d2ae7d05a629d17b7e707e249284b2e49358
breakshift <70482993+breakshift@users.noreply.github.com>
2020-09-02 11:49:41 -0300

    Fix broken tests

--- src/Switch/Switch.test.js
+++ src/Switch/Switch.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { ThemeProvider } from 'styled-components'
 import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 
@@ -7,28 +8,44 @@ import Switch from './Switch'
 import { ReactComponent as FlashIcon } from '../Demo/assets/flash.svg'
 import { ReactComponent as FlashOffIcon } from '../Demo/assets/flash-off.svg'
 
+const theme = {
+  switch: {
+    background: 'grey',
+    backgroundAccent: 'black',
+    // ...
+  }
+}
+
 describe('Switch', () => {
   it('should render on the screen', () => {
     render(
-      <Switch
-        label='Turn on speed mode'
-        activeStateIcon={<FlashIcon />}
-        inactiveStateIcon={<FlashOffIcon />}
-      />)
+      <ThemeProvider theme={theme}>
+        <Switch
+          label='Turn on speed mode'
+          activeStateIcon={<FlashIcon />}
+          inactiveStateIcon={<FlashOffIcon />}
+        />
+      </ThemeProvider>)
 
     const _switch = screen.getByRole('switch')
     expect(_switch).toBeInTheDocument()
   })
 
   it('should render a label when one is provided', () => {
-    render(<Switch label='Do not disturb' />)
+    render(
+      <ThemeProvider theme={theme}>
+        <Switch label='Do not disturb' />
+      </ThemeProvider>)
 
     const label = screen.getByText('Do not disturb')
     expect(label).toBeInTheDocument()
   })
 
   it('should toggle state when clicked', () => {
-    render(<Switch label='Show bookmarks bar' />)
+    render(
+      <ThemeProvider theme={theme}>
+        <Switch label='Show bookmarks bar' />
+      </ThemeProvider>)
 
     const _switch = screen.getByRole('switch')
     const label = screen.getByText('Show bookmarks bar')
@@ -42,14 +59,20 @@ describe('Switch', () => {
   })
 
   it('should be non-interactive if `disabled` is provided', () => {
-    render(<Switch disabled />)
+    render(
+      <ThemeProvider theme={theme}>
+        <Switch disabled />
+      </ThemeProvider>)
 
     const _switch = screen.getByRole('switch')
     expect(_switch).toBeDisabled()
   })
 
   it('should start with an active state if `defaultActive` is provided', () => {
-    render(<Switch defaultActive />)
+    render(
+      <ThemeProvider theme={theme}>
+        <Switch defaultActive />
+      </ThemeProvider>)
 
     const _switch = screen.getByRole('switch')
     expect(_switch).toHaveAttribute('aria-checked', 'true')
