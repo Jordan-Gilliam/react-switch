74d5ef0360f3581ad9763e39b5c50f78e605f097
breakshift <70482993+breakshift@users.noreply.github.com>
2020-09-07 16:44:31 -0300

    Add inline SVGs for testing

--- src/Switch/Switch.test.js
+++ src/Switch/Switch.test.js
@@ -4,16 +4,13 @@ import userEvent from '@testing-library/user-event'
 
 import Switch from './Switch'
 
-import { ReactComponent as FlashIcon } from '../Demo/assets/flash.svg'
-import { ReactComponent as FlashOffIcon } from '../Demo/assets/flash-off.svg'
-
 describe('Switch', () => {
   it('should render on the screen', () => {
     render(
       <Switch
         label='Turn on speed mode'
-        activeStateIcon={<FlashIcon />}
-        inactiveStateIcon={<FlashOffIcon />}
+        activeStateIcon={<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><path d="M194.82,496a18.36,18.36,0,0,1-18.1-21.53l0-.11L204.83,320H96a16,16,0,0,1-12.44-26.06L302.73,23a18.45,18.45,0,0,1,32.8,13.71c0,.3-.08.59-.13.89L307.19,192H416a16,16,0,0,1,12.44,26.06L209.24,489A18.45,18.45,0,0,1,194.82,496Z" /></svg>}
+        inactiveStateIcon={<svg xmlns="http://www.w3.org/2000/svg" width="512" height="512" viewBox="0 0 512 512"><path d="M432,448a15.92,15.92,0,0,1-11.31-4.69l-352-352A16,16,0,0,1,91.31,68.69l352,352A16,16,0,0,1,432,448Z" /><path d="M431.5,204A16,16,0,0,0,416,192H307.19L335.4,37.63c.05-.3.1-.59.13-.89A18.45,18.45,0,0,0,302.73,23L210.15,137.46a4,4,0,0,0,.29,5.35l151,151a4,4,0,0,0,5.94-.31l60.8-75.16A16.37,16.37,0,0,0,431.5,204Z" /><path d="M301.57,369.19l-151-151a4,4,0,0,0-5.93.31L83.8,293.64A16.37,16.37,0,0,0,80.5,308,16,16,0,0,0,96,320H204.83L176.74,474.36l0,.11A18.37,18.37,0,0,0,209.24,489l92.61-114.46A4,4,0,0,0,301.57,369.19Z" /></svg>}
       />)
 
     const _switch = screen.getByRole('switch')
