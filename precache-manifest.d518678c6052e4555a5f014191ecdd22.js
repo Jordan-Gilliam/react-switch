2cafdea3be61af0848b1028ab5e6d1530d591d00
breakshift <70482993+breakshift@users.noreply.github.com>
2020-09-12 17:50:28 -0300

    Updates

---
+++ precache-manifest.d518678c6052e4555a5f014191ecdd22.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d302f269ec9faffcb10754a683acd8",
+    "url": "/react-switch/index.html"
+  },
+  {
+    "revision": "aa380f5bc409b5929ddf",
+    "url": "/react-switch/static/css/main.778e39ca.chunk.css"
+  },
+  {
+    "revision": "76ae72e8f9b9736d0453",
+    "url": "/react-switch/static/js/2.3fb8ce3f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-switch/static/js/2.3fb8ce3f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa380f5bc409b5929ddf",
+    "url": "/react-switch/static/js/main.02025515.chunk.js"
+  },
+  {
+    "revision": "c96057f29e11c4b1ba66",
+    "url": "/react-switch/static/js/runtime-main.3ecc3585.js"
+  },
+  {
+    "revision": "601cb01d88a52d3b5396a4bd5fe6f31c",
+    "url": "/react-switch/static/media/flash-off.601cb01d.svg"
+  },
+  {
+    "revision": "0483a9217fc24ecf75cc9d3b63104f27",
+    "url": "/react-switch/static/media/flash.0483a921.svg"
+  }
+]);
